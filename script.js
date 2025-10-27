9b0aa63506485507a8e05e9363bb1418e38d2c91
Ammani-hash <66802969+Ammani-hash@users.noreply.github.com>
2025-10-26 17:44:42 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Optional: Slow scroll pause on hover
+const track = document.querySelector('.scroll-track');
+
+track.addEventListener('mouseenter', () => {
+  track.style.animationPlayState = 'paused';
+});
+
+track.addEventListener('mouseleave', () => {
+  track.style.animationPlayState = 'running';
+});
